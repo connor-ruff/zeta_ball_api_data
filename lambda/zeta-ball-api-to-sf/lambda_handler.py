538ee71a978aabc7d81c2c04fc6ef6f62fede9f4
connor-ruff <connorruff99@gmail.com>
2025-11-24 15:56:36 -0500

    LAMBDA UPDATE

--- lambda/zeta-ball-api-to-sf/lambda_handler.py
+++ lambda/zeta-ball-api-to-sf/lambda_handler.py
@@ -3,7 +3,10 @@ from utils import *
 
 def lambda_handler(event, context):
     
-    weeks_completed = calculate_weeks_since_season_start()
+    payload = json.loads(event["body"])["data"][0]
+    team_key = payload[1]
+    weeks_completed = payload[2]
+
 
     api_creds_obj = get_api_creds()
     access_token = get_access_token_from_refresh_token(
@@ -12,42 +15,33 @@ def lambda_handler(event, context):
         client_secret=api_creds_obj['client_secret']
     )
 
-    # Pull Matchup Data
-    matchup_data = get_matchups_for_week(
-        week_number=weeks_completed, 
-        access_token=access_token
-    )
-    drop_file_in_s3(
-        data=matchup_data,
-        bucket_name='connors-misc-blob-for-blobs',
-        file_key=f'zeta_ball/week_{weeks_completed}/matchups.json'
-    )
 
-    # Pull team stat data
-    for team_key in TEAM_KEYS:
-
-        team_stats_data = get_team_player_stats(
+    team_stats_data = get_team_player_stats(
             team_key, 
             weeks_completed, 
             access_token
-        )
+    )
 
-        drop_file_in_s3(
+    drop_file_in_s3(
             data=team_stats_data,
             bucket_name='connors-misc-blob-for-blobs',
             file_key=f'zeta_ball/week_{weeks_completed}/team_stats/{team_key}_stats.json'
-        )
+    )
 
-        matchup_stats_data = get_team_matchup_stats(team_key, access_token)
+    matchup_stats_data = get_team_matchup_stats(team_key, access_token)
 
-        drop_file_in_s3(
+    drop_file_in_s3(
             data=matchup_stats_data,
             bucket_name='connors-misc-blob-for-blobs',
             file_key=f'zeta_ball/week_{weeks_completed}/team_matchups/{team_key}_matchups.json'
-        )
+    )
 
     return {
-        'statusCode': 200,
-        'body': json.dumps("Matchup data fetched successfully."),
-        'weeks_completed': weeks_completed
+        "statusCode": 200,
+        "body": json.dumps({
+
+            "data": [
+               [0, "SUCCESS"]
+            ]
+        })
     }
